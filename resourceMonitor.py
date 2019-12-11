364d5dbb3f6a0cb8d3eacddfd83b8dcf41adc23f
Pad Lab <Mac4@pads-mac-mini-2.msft.pad.zone>
2019-12-10 17:01:20 -0700

    resource monitor updated to take command line parameter

--- resourceMonitor.py
+++ resourceMonitor.py
@@ -2,13 +2,17 @@ import psutil
 import logging
 import time
 import threading
+import sys
 monitoring=True
 def monitorSystem():
 	cpuUsage=[]
 	memUsage=[]
 	while monitoring:
-		cpuUsage.append(psutil.cpu_percent())
+		cpuUsage.append(psutil.cpu_percent(1))
+		print("Current CPU %: "+str(psutil.cpu_percent(1)))
 		memUsage.append(psutil.virtual_memory()[2])
+		print("Current memory stats are: ")
+		print(psutil.virtual_memory())
 		time.sleep(5)
 	totalCpu=0
 	totalMem=0
@@ -18,11 +22,14 @@ def monitorSystem():
 		totalMem+=item
 	avgMem=totalMem/(len(memUsage))
 	avgCPU=totalCpu/(len(cpuUsage))
-	print(avgCPU)
-	print(avgMem)
+	print("Average CPU Utilization was: "+str(avgCPU))
+	print("Average memory utilization was: "+ str(avgMem))
 
 if __name__=="__main__":
-	monitoringInterval=input("How many minutes do you want to monitor CPU and Memory usage?")
+	if sys.argv[1] != None:
+		monitoringInterval=sys.argv[1]
+	else:
+		monitoringInterval=input("How many minutes do you want to monitor CPU and Memory usage?")
 	t1=threading.Thread(target=monitorSystem, args=())
 	t1.start()
 	time.sleep(int(monitoringInterval)*60)
