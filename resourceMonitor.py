db45e58ecbc6a4096e0f26ac4a67171a8f6b842e
Edwards, Daniel <daniel.edwards@polycom.com>
2020-01-02 11:45:55 -0700

    disk added

--- resourceMonitor.py
+++ resourceMonitor.py
@@ -7,24 +7,32 @@ monitoring=True
 def monitorSystem():
 	cpuUsage=[]
 	memUsage=[]
+	diskUsage=[]
+	diskIo=[]
 	while monitoring:
 		cpuUsage.append(psutil.cpu_percent(1))
 		print("Current CPU %: "+str(psutil.cpu_percent(1)))
 		memUsage.append(psutil.virtual_memory()[2])
 		print("Current memory stats are: ")
 		print(psutil.virtual_memory())
+		print(psutil.disk_usage('/'))
+		diskUsage.append(psutil.disk_usage('/')[3])
 		time.sleep(5)
 	totalCpu=0
 	totalMem=0
+	totalDisk=0
 	for item in cpuUsage:
 		totalCpu+=item
 	for item in memUsage:
 		totalMem+=item
+	for item in diskUsage:
+		totalDisk+=item
 	avgMem=totalMem/(len(memUsage))
 	avgCPU=totalCpu/(len(cpuUsage))
+	avgDisk=totalDisk/(len(diskUsage))
 	print("Average CPU Utilization was: "+str(avgCPU))
 	print("Average memory utilization was: "+ str(avgMem))
-
+	print("Average disk usage was: "+str(avgDisk))
 if __name__=="__main__":
 	try:
 		monitoringInterval=sys.argv[1]
